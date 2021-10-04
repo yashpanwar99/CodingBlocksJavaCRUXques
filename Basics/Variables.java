c39edf37dba6540bedec0dd13dc171f76a2dac76
Yash1003 <yashpanwar99@gmail.com>
2021-10-05 01:17:44 +0530

    Basics 2nd

---
+++ Basics/Variables.java
@@ -0,0 +1,13 @@
+package Basics;
+// Variables are a lables assigned to the memory locations vwhere data is stored'
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        int d = a-b;
+
+    }
+
+}
